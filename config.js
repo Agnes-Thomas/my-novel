d92c9cc6d0c12e7acd5af307042f7edc181bd96e
Agnes-Thomas <agnesthomas2004@gmail.com>
2026-05-25 07:23:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,4 +5,4 @@
    STEP 2: Save. Done.
 ════════════════════════════════════════ */
 window.SUPABASE_URL      = 'https://jozxvmmsykuihnjmxuok.supabase.co/';
-window.SUPABASE_ANON_KEY = 'sb_publishable_eHFbKaLK624j6S7Bzi9rsA_p2PM_7sE';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impvenh2bW1zeWt1aWhuam14dW9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2NTY3NzksImV4cCI6MjA5NTIzMjc3OX0.l23Anzx-XuR1GCgtOVTx5crBKyoT2nuWIvlIKRJccLE';
